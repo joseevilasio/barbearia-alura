238d4f88b9f2827e687f32c223ad588134494946
joseevilasio <jose_evilasio@outlook.com>
2020-08-03 21:17:30 -0300

    Criando models Contatos e criando name em input

--- produtos/models.py
+++ produtos/models.py
@@ -8,3 +8,15 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.nome_produto
+
+
+class Contatos(models.Model):
+    nome = models.CharField(max_length=300)
+    email = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    mensagem = models.TextField()
+    contato = models.CharField(max_length=10)
+    turno_atendimento = models.CharField(max_length=10)
+    newsletter = models.BooleanField(default=False)
+
+
